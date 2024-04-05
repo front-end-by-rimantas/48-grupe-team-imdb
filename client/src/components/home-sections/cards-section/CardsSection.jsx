e0f8c73c53652322972046f043cdf71060b60efe
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-04-05 16:06:33 +0300

    top and middle main initial completion

--- client/src/components/home-sections/cards-section/CardsSection.jsx
+++ client/src/components/home-sections/cards-section/CardsSection.jsx
@@ -1,7 +1,102 @@
+import { useRef } from 'react';
 import style from './CardsSection.module.css'
-
+import avatar from '../../../assets/images/avatar.jpg'
+import titanic from '../../../assets/images/titanic.jpg'
+import crash from '../../../assets/images/crash.jpg'
+import pulpfiction from '../../../assets/images/pulpfiction.jpg'
+import se7en from '../../../assets/images/se7en.jpg'
+import thetrumanshow from '../../../assets/images/thetrumanshow.jpg'
 export function CardsSection() {
+    const movieListRef = useRef(null);
+
+    const handlePrevClick = () => {
+        movieListRef.current.scrollLeft -= 250; // Adjust the scroll distance as needed
+    };
+
+    const handleNextClick = () => {
+        movieListRef.current.scrollLeft += 250; // Adjust the scroll distance as needed
+    };
+
     return (
-        <section className={style.cardsSection}>MIDDLE SIDE</section>
-    )
+        <section className={style.cardsSection}>
+            <button className={style.prevButton} onClick={handlePrevClick}>&lt;</button>
+            <div className={style.movieContainer} ref={movieListRef}>
+                <ul className={style.movieList}>
+                    <li className={style.movieCard}>
+                        <img src={avatar} alt="Avatar" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>Avatar</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={titanic} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>titanic</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={crash} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>crash</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={pulpfiction} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>pulpfiction</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={se7en} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>se7en</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={thetrumanshow} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>thetrumanshow</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={thetrumanshow} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>thetrumanshow</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={thetrumanshow} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>thetrumanshow</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    <li className={style.movieCard}>
+                        <img src={thetrumanshow} alt="Movie Title" className={style.movieImage} />
+                        <div className={style.movieInfo}>
+                            <div className={style.movieRating}><span>&#9733;</span>8.5</div>
+                            <div className={style.movieName}>thetrumanshow</div>
+                            <button className={style.trailerButton}>Watch Trailer</button>
+                        </div>
+                    </li>
+                    </ul>
+            </div>
+            <button className={style.nextButton} onClick={handleNextClick}>&gt;</button>
+        </section>
+    );
 }
--- client/src/components/home-sections/hero-section/HeroSection.jsx
+++ client/src/components/home-sections/hero-section/HeroSection.jsx
@@ -1,7 +1,22 @@
+/* eslint-disable react/prop-types */
 import style from './HeroSection.module.css'
+import posterUrl from '../../../assets/images/avatar.jpg'
 
 export function HeroSection() {
+   
+
     return (
-        <section className={style.heroSection}>TOP SIDE</section>
+        <section className={style.heroSection}>
+             <div className={style.movieInfo}>
+                <div className={style.movieName}>Avatar</div>
+                <div className={style.poster}>
+                    <img src={posterUrl} alt="" />
+                </div>
+            </div>
+            <div className={style.arrowButtons}>
+                <button className={style.prevButton}>{'<'}</button>
+                <button className={style.nextButton}>{'>'}</button>
+            </div>
+        </section>
     )
 }
