4cf66b32146f6daa19f79cb4f900fd5fb0bbc416
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-04-08 21:00:08 +0300

    Added logo in header and small style changes in sign in page

--- client/src/components/logo/LogoImdb.jsx
+++ client/src/components/logo/LogoImdb.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import headerLogo from "../../assets/images/logo/imdb_logo.png";
+import style from "./LogoImdb.module.css";
 
 export function LogoImdb() {
     return (
-        <Link  to="/"><img src="" alt="" />Logo</Link>
+        <Link to="/"><img className={style.logo} src={headerLogo} alt="Logo" /></Link>
     )
 }
