1cb10ae932e6f3234b6838fb46ae517a80a69976
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-04-10 20:40:04 +0300

    backend get object from json from using req.params

--- server/controllers/movies-controller.js
+++ server/controllers/movies-controller.js
@@ -17,3 +17,4 @@ export const getMovies = (req, res) => {
 // export const addMovie = (req, res) => {}  
 // export const updateMovie = (req, res) => {}
 // export const deleteMovie = (req, res) => {}
+
