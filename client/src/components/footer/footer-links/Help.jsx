e0602cc82c33669afef3a07e9c0690b6a1c93d57
Aina <ainaeinikyte@gmail.com>
2024-04-12 19:44:55 +0300

    feat: img help

--- client/src/components/footer/footer-links/Help.jsx
+++ client/src/components/footer/footer-links/Help.jsx
@@ -6,12 +6,35 @@ export function Help() {
       <section className={style.container}>
         <div className={style.helpArea}>
           <div className={style.title}>
-            <img src={"http://localhost:4840/assets/images/whitelogo.png"} className={style.whiteLogo}/>
+            <img
+              src={"http://localhost:4840/assets/images/whitelogo.png"}
+              className={style.whiteLogo}
+            />
             <h2 className={style.text}>Help Center</h2>
           </div>
         </div>
       </section>
-      <section></section>
+      <section className={style.containerTwo}>
+        <div>
+          <div>
+            <img src={"http://localhost:4840/assets/images/prologo.png"} />
+            <h4>For industry professionals</h4>
+            <p>Make the most of your Pro membership benefits</p>
+          </div>
+          <div>
+            <img src={"http://localhost:4840/assets/images/prologo.png"} />
+            <h4>For entertainment fans</h4>
+            <p>Learn how to discover what to watch</p>
+          </div>
+          <div>
+            <img
+              src={"http://localhost:4840/assets/images/contributorslogo.png"}
+            />
+            <h4>For contributors</h4>
+            <p>Learn about contributing & updating data</p>
+          </div>
+        </div>
+      </section>
       <section></section>
     </div>
   );
