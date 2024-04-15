93e290a9a7d5ed45bf633c2f7990113854dc090b
Aina <ainaeinikyte@gmail.com>
2024-04-15 20:42:16 +0300

    feat: search movies in API

--- server/router/movies.js
+++ server/router/movies.js
@@ -1,12 +1,15 @@
 import express from "express";
-import fs from "fs";
-import { getMovies} from "../controllers/movies-controller.js";
-import { getMovie} from "../controllers/movies-controller.js";
+import {
+  getMovies,
+  searchMovies,
+  getMovie,
+} from "../controllers/movies-controller.js";
 
 const router = express.Router();
 
 // router.get('/add', addMovie)  
 router.get("/get", getMovies);
+router.get("/search", searchMovies);
 // router.patch('/update', updateMovies)
 // router.delete('/delete', deleteMovies)
 router.get("/get/:href", getMovie);
