38b29b38d0a70a0a59b4f700a643785c0bb05dd7
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-04-19 13:07:24 +0300

    Update header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
-import { SearchBar } from '../search/SearchBar'
+import { SearchBar } from '../search/SearchBar';
 import { LogoImdb } from '../logo/LogoImdb';
 import { GlobalContext } from '../../context/GlobalContext';
 import { useContext, useState } from 'react';
@@ -8,18 +8,18 @@ import { LuMenuSquare } from "react-icons/lu";
 import { IoClose, IoSearch } from "react-icons/io5";
 
 export function Header() {
-const {loginStatus, updateLoginStatus} = useContext(GlobalContext);
-const [menuOpen, setMenuOpen] = useState(false);
-const [searchOpen, setSearchOpen] = useState(true);
+    const {loginStatus, updateLoginStatus} = useContext(GlobalContext);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [searchOpen, setSearchOpen] = useState(true);
 
-const searchBtn = (<span className={style.searchBtn}><IoSearch /></span>);
-const closeSearchBtn = (<span className={style.closeSearchBtn}><IoClose /></span>);
+    const searchBtn = (<span className={style.searchBtn}><IoSearch /></span>);
+    const closeSearchBtn = (<span className={style.closeSearchBtn}><IoClose /></span>);
 
-const openMenuBtn = (<span className={style.menuBtn}><LuMenuSquare /></span>);
-const closeMenuBtn = (<span className={style.closeMenuBtn}><LuMenuSquare /></span>);
+    const openMenuBtn = (<span className={style.menuBtn}><LuMenuSquare /></span>);
+    const closeMenuBtn = (<span className={style.closeMenuBtn}><LuMenuSquare /></span>);
 
-const signInButton = (<Link className={style.navLink} to="/sign-in">Sign In</Link>);
-const signOutButton = (<button onClick={handleSignOut} className={style.navLink}>Sign out</button>);
+    const signInButton = (<Link className={style.navLink} to="/sign-in">Sign In</Link>);
+    const signOutButton = (<button onClick={handleSignOut} className={style.navLink + ' ' + style.signOutBtn}>Sign out</button>);
 
     function handleSignOut() {
         updateLoginStatus(false);
@@ -34,8 +34,12 @@ const signOutButton = (<button onClick={handleSignOut} className={style.navLink}
                 <SearchBar />
             </div>
             <div className={style.menuBtnBox}>
-                <span className={searchOpen ? style.searchBtn : style.searchBtnClose} onClick={() => {setSearchOpen(!searchOpen)}}>{searchOpen ? searchBtn : closeSearchBtn}</span>
-                <span className={searchOpen ? style.menuBtn : style.menuBtnClose} onClick={() => {setMenuOpen(!menuOpen)}} >{menuOpen ? openMenuBtn : closeMenuBtn}</span>
+                <span className={searchOpen ? style.searchBtn : style.searchBtnClose} 
+                    onClick={() => {setSearchOpen(!searchOpen)}}>{searchOpen ? searchBtn : closeSearchBtn}
+                </span>
+                <span className={searchOpen ? style.menuBtn : style.menuBtnClose} 
+                    onClick={() => {setMenuOpen(!menuOpen)}} >{menuOpen ? openMenuBtn : closeMenuBtn}
+                </span>
             </div>
             <div className={menuOpen ? style.navLinkBox : style.navLinkBoxClose}>
                 <nav>
@@ -45,6 +49,6 @@ const signOutButton = (<button onClick={handleSignOut} className={style.navLink}
                 </nav>
             </div>
     </header>
-    )
+    );
 }
 
