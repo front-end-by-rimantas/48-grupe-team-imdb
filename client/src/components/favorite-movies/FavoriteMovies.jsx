ff5e2665b43542eb02bda3997c3e2022281356f2
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-04-23 21:49:19 +0300

    User can have favorite movie in list

--- client/src/components/favorite-movies/FavoriteMovies.jsx
+++ client/src/components/favorite-movies/FavoriteMovies.jsx
@@ -1,19 +1,43 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-key */
-/* eslint-disable no-unused-vars */
-import { useContext } from 'react'
+
+import { useContext, useState } from 'react'
 import style from './FavoriteMovies.module.css'
 import { GlobalContext } from '../../context/GlobalContext'
 import { Link } from 'react-router-dom'
 
-export function FavoriteMovies() {
-    const { favorite, updateFavoriteStatus } = useContext(GlobalContext)
 
-    console.log(favorite)
+export function FavoriteMovies() {
+    const { favorite, userId } = useContext(GlobalContext);
+    const [delet, setDelet] = useState(false);
+
+    const favoriteMoviesHrefArr = [];
+
+    for (const data of favorite) {
+        if (data.userId === userId) {
+            favoriteMoviesHrefArr.push(data);
+        }
+    } 
+
+    function handleDelete (favorit) {
+        setDelet(!delet);
+        console.log(favorit);
+        
+    }
+
     return (
         <div className={style.container}>
-            {favorite.map((favorit, index) => ( < Link to={`/movies/get/${favorit}`}>
-                <img className={style.imgItem} src={`http://localhost:4840/assets/images/${favorit}.jpg`} alt="" />
-                </Link>))}
+            <h1 className={style.favoritMoviesTitle}>List of my favorite movies</h1>
+            <div className={style.favoritMoviesBox}>
+                {favoriteMoviesHrefArr.map((favorit, index) => (
+                    <div key = {index} className={style.favoriteMovieCard}>
+                    < Link to={`/movies/get/${favorit.href}`}>
+                        <img className={style.cardImg} src={`http://localhost:4840/assets/images/${favorit.href}.jpg`} alt="" />
+                    </Link>
+                    <button onClick={() => handleDelete(favorit.id)} className={style.deleteBtn}>DELETE</button>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
--- client/src/components/movie-list/MovieItemInner.jsx
+++ client/src/components/movie-list/MovieItemInner.jsx
@@ -12,18 +12,31 @@ import { MovieItemTable } from './responsive-design/MovieItemTable';
 import { GlobalContext } from '../../context/GlobalContext';
 
 
+
 export function MovieItemInner() {
     const { href } = useParams();
     const [movie, setMovie] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const {favorite, favoriteStatus, updateFavoriteData, updateFavoriteStatus } = useContext(GlobalContext);
+    const {userId, favorite, favoriteStatus, loginStatus, updateFavoriteData, updateFavoriteStatus } = useContext(GlobalContext);
     const [favorit, setFavorite] = useState(false);
+    
+    const favoriteMoviesHrefArr = [];
+
+    for (const data of favorite) {
+        if (data.userId === userId) {
+            favoriteMoviesHrefArr.push(data.href);
+        }
+    } 
 
     const activeFavoriteBtn = (<span className={style.favoriteIconActive}><MdFavorite/></span>);
     const inactiveFavoriteBtn = (<span className={style.favoriteIconInactive}><MdFavorite/></span>);
+    const favoriteBtn = (
+        <button className={style.favoriteBtn}  onClick={handleFavorite} >
+        {favoriteMoviesHrefArr.includes(href) ? activeFavoriteBtn : inactiveFavoriteBtn}
+        </button>
+    );
 
-    console.log(href, favorite)
     
     useEffect(() => {
         axios.get(`http://localhost:4840/movies/get/${href}`)
@@ -57,12 +70,12 @@ export function MovieItemInner() {
                 body: JSON.stringify({
                     href,
                     favorit,
-
+                    userId,
                 }),
             })
                 .then(res => res.json())
                 .then(data => {
-                    updateFavoriteData(data.arr),
+                    updateFavoriteData(data.favoriteArr),
                     updateFavoriteStatus(data.statusOffFavorite)
                     
                 })
@@ -73,65 +86,63 @@ export function MovieItemInner() {
           <main style={{
         background: 'linear-gradient(90deg, rgb(var(--ipt-baseAlt-shade1-rgb, 31,31,31)), 20%, rgba(var(--ipt-baseAlt-shade1-rgb, 31,31,31), 0.6), 80%, rgb(var(--ipt-baseAlt-shade1-rgb, 31,31,31)))'
     }}>  
-                        <MediaQuery maxWidth={640}> 
-                            {(matches) =>
-                            matches ? (
-                                <MovieItemMobile movie={movie} />
-                                ):(
-                        <MediaQuery minWidth={641} maxWidth={1276}>
-                            {(matches) =>
-                            matches ? (
-                                <MovieItemTable movie={movie} />
-                            ):(
-                                    <div className={`${style.boss} ${style.containerInner}`}>
-                                    <div className={style.heroSection}>
-                                        <div className={style.nameAndRating}>
-                                            <h1>{movie?.name}</h1>
-                                            <div className = {style.underName}>
-                                                <p>{movie?.year}</p>
-                                                        <svg width="20" height="20">
-                                                            <circle cx="10" cy="10" r="3" fill= "white" />
-                                                        </svg>
-                                                <p>{movie?.ageCenzor}</p>
-                                                        <svg width="20" height="20">
-                                                            <circle cx="10" cy="10" r="3" fill= "white" />
-                                                        </svg>
-                                                        <button className={style.favoriteBtn}  onClick={handleFavorite} >
-                                                            {favorite.includes(href)? activeFavoriteBtn : inactiveFavoriteBtn}
-                                                        </button>
-                                            </div>
-                                        </div>
-                                        <div className={style.rating}>
-                                            <div>
-                                                <p>IMDB RATING</p>
-                                                <div className={style.yellow}>
-                                                    <i className={style.yellowStar}><GoStarFill size="1.5rem" /> </i>
-                                                    <p>{movie?.rating}/10</p>
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <p>YOUR RATING</p>
-                                                <div className={style.blue}>
-                                                    <i className={style.blueStar}><CiStar size="1.5rem" /> </i>
-                                                    <p className={style.blueRate}>Rate</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className={style.containerItemInner}>
-                                            <img className={style.imgItem} src={`http://localhost:4840/assets/images/${movie?.path}`} alt="" />
-                                            <iframe className={style.url} src={movie?.url} title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
-                                    </div>
-                                    <div className={style.descriptionMovie}>
-                                        {movie?.description?.split('\n').map((line, index) => (
-                                            <div key={index}>{line}</div>
-                                        ))}
-                                    </div>
-                                </div>
-                           )}
-                         </MediaQuery>
-                        )}
-                </MediaQuery>
-        </main>
-    </>);
+    <MediaQuery maxWidth={640}> 
+        {(matches) =>
+        matches ? (
+            <MovieItemMobile movie={movie} />
+            ):(
+    <MediaQuery minWidth={641} maxWidth={1276}>
+        {(matches) =>
+        matches ? (
+            <MovieItemTable movie={movie} />
+        ):(
+                <div className={`${style.boss} ${style.containerInner}`}>
+                <div className={style.heroSection}>
+                    <div className={style.nameAndRating}>
+                        <h1>{movie?.name}</h1>
+                        <div className = {style.underName}>
+                            <p>{movie?.year}</p>
+                                    <svg width="20" height="20">
+                                        <circle cx="10" cy="10" r="3" fill= "white" />
+                                    </svg>
+                            <p>{movie?.ageCenzor}</p>
+                                    <svg width="20" height="20">
+                                        <circle cx="10" cy="10" r="3" fill= "white" />
+                                    </svg>
+                                    {loginStatus ? favoriteBtn : null}
+                        </div>
+                    </div>
+                    <div className={style.rating}>
+                        <div>
+                            <p>IMDB RATING</p>
+                            <div className={style.yellow}>
+                                <i className={style.yellowStar}><GoStarFill size="1.5rem" /> </i>
+                                <p>{movie?.rating}/10</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>YOUR RATING</p>
+                            <div className={style.blue}>
+                                <i className={style.blueStar}><CiStar size="1.5rem" /> </i>
+                                <p className={style.blueRate}>Rate</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.containerItemInner}>
+                        <img className={style.imgItem} src={`http://localhost:4840/assets/images/${movie?.path}`} alt="" />
+                        <iframe className={style.url} src={movie?.url} title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                </div>
+                <div className={style.descriptionMovie}>
+                    {movie?.description?.split('\n').map((line, index) => (
+                        <div key={index}>{line}</div>
+                    ))}
+                </div>
+            </div>
+       )}
+     </MediaQuery>
+    )}
+</MediaQuery>
+</main>
+</>);
 }
