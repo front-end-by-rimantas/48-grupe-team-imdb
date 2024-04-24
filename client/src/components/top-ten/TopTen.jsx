ae4e1636187ef25307816b0ffdba27a181cf3608
Lukas Naujunas <l.naujunas@gmail.com>
2024-04-24 11:29:33 +0300

    second commit

--- client/src/components/top-ten/TopTen.jsx
+++ client/src/components/top-ten/TopTen.jsx
@@ -1,7 +1,23 @@
-import style from './TopTen.module.css'
+import style from './TopTen.module.css';
+import { MovieTop10 } from '../movie-list/MovieTop10';
 
 export function TopTen() {
+
     return (
-        <div className={style.container}>Top 10</div>
-    )
+        <main className={style.allElements}>
+            <div className={style.topSide}>
+                <h1 className={style.text}>IMDb Charts</h1>
+                <p className={style.border}>IMDb Top 10 Movies</p>
+                <p className={style.text}>As rated by regular IMDb voters.</p>
+            </div>
+            <div className={style.topSide}>
+                <p className={style.text}>10 Titles</p>
+            </div>
+            <div className={style.movieCards}>
+                <ul className={style.cardsContainer}>
+                    <MovieTop10 className={style.mainCards} />
+                </ul>
+            </div>
+        </main>
+    );
 }
