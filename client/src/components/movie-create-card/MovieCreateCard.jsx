3212b494477e569244fb5e5189559cbed7d104c2
Aina <ainaeinikyte@gmail.com>
2024-04-26 11:22:13 +0300

    feat: initial css

--- client/src/components/movie-create-card/MovieCreateCard.jsx
+++ client/src/components/movie-create-card/MovieCreateCard.jsx
@@ -1,5 +1,54 @@
-import style from './MovieCreateCard.module.css';
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo/imdb_logo.png";
+import style from "./MovieCreateCard.module.css";
 
 export function MovieCreateCard() {
-    return <></>
+  return (
+    <div className={style.main}>
+      <div className={style.logoBox}>
+        <Link to="/">
+          <img className={style.registrationLogo} src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <div className={style.form}>
+        <span className={style.titleF}>
+          <h1 className={style.titleF}>Create movie</h1>
+        </span>
+        <form className={style.context}>
+          <div className={style.formRow}>
+            <label className={style.label} htmlFor="">
+              Movie title
+            </label>
+            <input className={style.inputForm} type="text" placeholder="" />
+          </div>
+          <div className={style.formRow}>
+            <label className={style.label} htmlFor="">
+              Year
+            </label>
+            <input className={style.inputForm} type="number" placeholder="" />
+          </div>
+          <div className={style.formRow}>
+            <label className={style.label} htmlFor="">
+              Category
+            </label>
+            <input className={style.inputForm} type="text" placeholder="" />
+          </div>
+          <div className={style.formRow}>
+            <label className={style.label} htmlFor="">
+              Age Cenzor
+            </label>
+            <input className={style.inputForm} type="text" placeholder=" " />
+          </div>
+          <div className={style.formRow}>
+            <button
+              className={`${style.button} ${style.textButton}`}
+              type="submit"
+            >
+              Create your movie
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
